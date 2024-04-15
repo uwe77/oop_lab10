faf1c3a853c861cdfc944235872751174bf5dead
uwe77 <uwe90711@gmail.com>
2024-04-15 21:03:56 +0800

    fix: changing lab for phoebe

--- scripts/play_lunarlander.py
+++ scripts/play_lunarlander.py
@@ -9,7 +9,8 @@ import add_path
 import warnings
 warnings.filterwarnings("ignore")
 
-env = gym.make('custom_gymnasium.envs:CustomLunarLander-v1', gravity= -5.0, turbulence_power = 1, render_mode="rgb_array")
+
+env = gym.make('custom_gymnasium.envs:CustomLunarLander-v1', gravity= -4.0, turbulence_power = 1, render_mode="rgb_array")
 env.reset()
 env.render()
 def callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
@@ -17,10 +18,11 @@ def callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
     	print("\x1b[6;30;42mSuccess!!\x1b[0m") if rew==100 else print("Crashed!!")   
     return [rew]
 plotter = PlayPlot(callback, 150, ["reward"])
-play(env,callback = plotter.callback
-                        ,keys_to_action={
-                          "w": 2,
-                          "a": 1,
-                          "d": 3,
-                          }, noop=0)
+play(env,callback = plotter.callback,
+                    keys_to_action={
+                        'w': 2,
+                        'a': 1,
+                        'd': 3,
+                    },
+                    noop=0)
 
